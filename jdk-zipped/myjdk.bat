059f2bf28bebafb373358be71ec81b5f8c907251
Sudhaker Raj <sudhaker@gmail.com>
2017-05-02 20:22:07 -0400

    Add files via upload

---
+++ jdk-zipped/myjdk.bat
@@ -0,0 +1,19 @@
+
+set INSTALL=jdk-8u131-windows-x64.exe
+set MYJDK=jdk1.8.0_131
+
+7z.exe e %INSTALL% .rsrc\1033\JAVA_CAB9\110 .rsrc\1033\JAVA_CAB10\111
+
+7z.exe e 110 -o%MYJDK%
+
+7z.exe e 111
+7z.exe x tools.zip -o%MYJDK%
+
+del 110 111 tools.zip
+
+for /r %MYJDK% %%x in (*.pack) do %MYJDK%\bin\unpack200 -r "%%x" "%%~dx%%~px%%~nx.jar"
+
+7z.exe a -tzip %MYJDK%.zip %MYJDK%
+
+rmdir /q /s %MYJDK%
+
