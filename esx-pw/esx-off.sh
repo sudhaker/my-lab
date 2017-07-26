c2f7336f7e970f89a1a10117e3806d99c96d7a27
Sudhaker Raj <sudhaker@gmail.com>
2017-07-26 18:36:46 -0400

    Create esx-off.sh

---
+++ esx-pw/esx-off.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $(basename $0) n1|n2|n3|all"
+	exit
+fi
+
+PW_STATE=off
+
+for N in "$@"
+do
+	if [ "$N" == "all" ] || [ "$N" == "n1" ]; then
+		ipmitool -H 192.168.20.11 -U root -P root chassis power $PW_STATE 2> /dev/null
+		sleep 5
+	fi
+
+	if [ "$N" == "all" ] || [ "$N" == "n2" ]; then
+		ipmitool -H 192.168.20.12 -U root -P root chassis power $PW_STATE 2> /dev/null
+		sleep 5
+	fi
+
+	if [ "$N" == "all" ] || [ "$N" == "n3" ]; then
+		ipmitool -H 192.168.20.13 -U root -P root chassis power $PW_STATE 2> /dev/null
+		sleep 5
+	fi
+done
